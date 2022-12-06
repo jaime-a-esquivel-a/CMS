a7816a176f1ce6709b9acebaf6f7ea32f3c8426a
Jaime Alberto Esquivel Acosta <jaimeesquivel@Jaimes-MacBook-Pro.local>
2022-12-06 16:37:12 -0600

    seed.sql updated

--- db/seed.sql
+++ db/seed.sql
@@ -8,11 +8,12 @@ INSERT INTO role (title, salary, department_id)
 VALUES
     ("Chief Marketing Officer", 80000, 1),
     ("Chief Sales Officer", 90000, 2),
+    ("Sales Strategist", 90000, 2),
     ("Chief Information Officer", 95000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
     ("Ivan", "Palacios", 1, NULL),
     ("Roberto", "Huerta", 2, NULL),
-    ("Jaime", "Esquivel", 3, NULL);
+    ("Jaime", "Esquivel", 4, NULL);
 
